8ba3e9548529872357e01d7be675f46e5555dfed
David Poirier-Quinot <davipoir@gmail.com>
2017-06-20 10:24:22 +0200

    fixed get mouse click pos in browser (not smartphone)

--- src/client/player/PlayerExperience.js
+++ src/client/player/PlayerExperience.js
@@ -147,7 +147,7 @@ export default class PlayerExperience extends soundworks.Experience {
     // handle click scenario (id is then a MouseEvent, normX and normY are not defined)
     if( id.button !== undefined ){
       let event = id;
-      normY = event.screenY / event.view.innerHeight;
+      normY = event.clientY / event.view.innerHeight;
     }
     // fade off image
     const imageFadeOffDuration = 1;
